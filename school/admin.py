c1aa4ae9e1ca1b56be580dc53c516f9f4095d215
MauricioMSoares <mauricio.soares@sptech.school>
2024-02-23 23:01:10 -0300

    feat: created an object serializer and configured django admin access

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from school.models import Student, Course
 
-# Register your models here.
+class Students(admin.ModelAdmin):
+    list_display = ('id', 'name', 'rg', 'cpf', 'birth_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 20
+
+admin.site.register(Student, Students)
+
+class Courses(admin.ModelAdmin):
+    list_display = ('id', 'code', 'desc')
+    list_display_links = ('id', 'code')
+    search_fields = ('code',)
+
+admin.site.register(Course, Courses)
